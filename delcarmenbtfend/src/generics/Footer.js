7e47ba1f9a8f4eb9f846f5106672a4cad32340ac
DavidMaldonado0 <eduardodavidmm@hotmail.com>
2019-04-01 20:56:42 -0600

    frontend design

---
+++ delcarmenbtfend/src/generics/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+      <p> Seminario de Software 2019 01 | David Maldonado | Franco Fuentes | Gabriel Mora | Leonardo Ochoa | Jeancarlos Lainez. </p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
